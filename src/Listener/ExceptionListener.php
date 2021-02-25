78b418e18ec1724266d1ca94948b2e1af23b38ca
AMBROSINI Loïc <crazyball@crazyball.fr>
2021-02-25 17:48:59 +0100

    add: exceptionListener for Json formated unhandled exceptions

---
+++ src/Listener/ExceptionListener.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $message = json_encode([
+            'status' => $exception->getCode(),
+            'message' => $exception->getMessage()
+        ]);
+
+        $response = new Response();
+        $response->setContent($message);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+}
